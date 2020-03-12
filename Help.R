99a24f8fe5512f2cd61e071130dba88cd1ef31d6
Thomas Brinkman <tj.brinkman@gmail.com>
2020-03-12 12:54:45 +0100

    re #2

--- Help.R
+++ Help.R
@@ -1,6 +1,5 @@
 for (i in 1:10) {
-  print('Help, er is iets stuk!')
-  print('Alles gaat hartstikke goed')
+  print('Spongebob')
 }
 print('Thomas neemt dit project niet serieus.')
 print('Er is niks aan de hand')
