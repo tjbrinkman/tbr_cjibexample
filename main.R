134408fd0b6217ce7ba246451feea12c30c04079
Gerben Kooistra <gerben_kooistra@live.nl>
2020-03-12 16:24:59 +0100

    Cache toegevoegd

--- main.R
+++ main.R
@@ -2,3 +2,27 @@
 
 # Laden van de packages
 source('scripts/load_packages.R')
+
+# Dit duurt even
+conf <- yaml::read_yaml("config/passwords.yml")
+
+db <- mongo(collection = "almereparkingjson",
+            url = sprintf(
+              "mongodb://%s:%s@%s/%s",
+              conf$database$login, 
+              conf$database$password, 
+              "ds229186.mlab.com:29186",
+              "almereparking"))
+
+slow_process <- function(){
+  parking <- db$find()
+}
+
+fn <- "cache/slow_object.rds"
+
+if(!file.exists(fn)){
+  slow_object <- slow_process()
+  saveRDS(slow_object, fn)
+} else {
+  slow_object <- readRDS(fn)
+}
