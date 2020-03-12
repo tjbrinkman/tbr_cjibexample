aaa17d01e03f2fac65b3861536c5904f44e3087b
njvdvelde <n.vandervelde@cjib.nl>
2020-03-12 15:00:55 +0100

    nog meer

--- scripts/load_packages.R
+++ scripts/load_packages.R
@@ -1,13 +1,13 @@
+# Laden packages
+
 library(mongolite)
 library(randomcoloR)
 library(ggplot2)
 library(plotly)
 library(lubridate)
 library(dplyr)
-library(lubridate)
 library(readxl)
 library(leaflet)
-library(lubridate)
 library(randomForest)
 library(randomForestExplainer)
 library(mgcv)
